c4266ec5b7a7a16994e8a7830c8380fd02c1f2b6
Rafael Morais <rafamorais.br@live.com>
2020-11-14 12:25:44 -0300

    first commit

---
+++ linuxamazon-ec2aspnetcore/Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace linuxamazon_ec2aspnetcore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Rafael", "Teste", "Amazon", "AWS", "EC2", "ASPNETCORE", "3.1"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string[] Get()
+        {
+            var rng = new Random();
+            return Summaries;
+        }
+    }
+}
