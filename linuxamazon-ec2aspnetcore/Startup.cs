f266e0a6d4e1bc6c3f21d7982ac258658bc8c21a
Rafael Morais <rafamorais.br@live.com>
2020-11-16 22:48:29 -0300

    teste ec2

--- linuxamazon-ec2aspnetcore/Startup.cs
+++ linuxamazon-ec2aspnetcore/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -36,9 +37,14 @@ namespace linuxamazon_ec2aspnetcore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
 
             app.UseAuthorization();
 
