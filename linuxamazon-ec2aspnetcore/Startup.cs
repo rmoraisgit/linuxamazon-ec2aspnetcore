edb67a4816999b4a534785e01c4276d850b345b9
Rafael Morais <rafamorais.br@live.com>
2020-11-24 22:56:33 -0300

    test

--- linuxamazon-ec2aspnetcore/Startup.cs
+++ linuxamazon-ec2aspnetcore/Startup.cs
@@ -27,6 +27,12 @@ namespace linuxamazon_ec2aspnetcore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder => builder.AllowAnyOrigin());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
